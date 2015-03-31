f922352f2b24d8cab080f6450ac225d9f4e926a1
wassy <d.voda94@gmail.com>
2015-03-31 17:06:56 +0200

    Update Functions.cpp and add coments

--- Project/Project/Functions.cpp
+++ Project/Project/Functions.cpp
@@ -22,13 +22,12 @@ Costumer * Queue::get(){
 }
 
 void firstInit(Market * market){
-	market->costumers = 0;
-	market->costumersInQueue = 0;
+	market->costumers = market->costumersInQueue = 0;
+	/*Initialize all cashdesks*/
 	for (size_t i = 0; i < NUM_CASHDESKS; i++){
 		market->cashDesks[i] = new Queue;
-		market->cashDesks[i]->head = 0;
-		market->cashDesks[i]->tail = 0;
-		market->cashDesks[i]->numberOfItems = 0;
+		market->cashDesks[i]->head = market->cashDesks[i]->tail = market->cashDesks[i]->numberOfItems = 0;	
+		/*Initialize all queues*/
 		for (size_t j = 0; j < MAX_COSTUMERS; j++){
 			market->cashDesks[i]->costumers[j] = new Costumer;
 		}
@@ -37,6 +36,7 @@ void firstInit(Market * market){
 }
 
 void createNewCostumer(Market * market){
+	/*Were generated all?*/
 	if (market->costumers == MAX_COSTUMERS)
 		return;
 	Costumer * newCostumer = new Costumer();
@@ -61,14 +61,18 @@ int findShortestQueue(Market * market){
 
 void onSales(Market * market){
 	for (size_t i = 0; i < NUM_CASHDESKS; i++){
+		/*Exist costumer in queue?*/
 		if (market->cashDesks[i]->numberOfItems > 0){
 			int head = market->cashDesks[i]->head;
 			Costumer * currentCostumer = market->cashDesks[i]->costumers[head];
+			/*Are items in costumers  basket?*/
 			if (currentCostumer->products > 0){
+				/*Remove one item from costumers basket process*/
 				currentCostumer->products--;
 				cout << "K" << currentCostumer->id << "[" << currentCostumer->products << "] ";
 			}
 			else{
+				/*Remove costumer from queue process*/
 				market->cashDesks[i]->get();
 				int queueCostumers = market->cashDesks[i]->numberOfItems;
 				market->costumersInQueue--;
